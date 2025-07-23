8b836be15bba238cb2c77b660c9e63af7ff57b80
best0994207165 <best0994207165@gmail.com>
2025-07-23 19:36:08 +0700

    Delete 68143206013-2/HW02_05_68143206013-2.py

--- 68143206013-2/HW02_05_68143206013-2.py
+++
@@ -1,9 +0,0 @@
-UserName = input("กรอกชื่อ :")
-FixUser = UserName.strip().lower().replace('_','')
-dflen = len(FixUser)
-
-if   5 <= dflen <= 20:
-    print(f"ใช้ได้ {FixUser}")
-else:
-    print("ชื่อต้องมีความยาวระหว่าง 5-20 ตัวอักษร")
-    input("กด enter เพื่อปิดโปรแกรม :")
